3b087708be5226a6959459cf94f5a537f433d325
Apurv701 <157449850+Apurv701@users.noreply.github.com>
2024-01-23 11:20:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+const connectDB = require('./config/db');
+const path = require('path');
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+// init middleware
+app.use(express.json({ extended: false }));
+// Connect Database
+connectDB();
+
+app.use(cors())
+
+//Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => { res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) });
+}
+
+
+
+
+app.listen(PORT, () => { console.log(`Server Started on port ${PORT}`) });
